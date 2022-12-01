93cf0b4f1dbbc3a4663418a49e2ff411c8150f01
Nathan Treadaway <natetread@gmail.com>
2022-12-01 11:46:32 -0500

    solutions for prac 1

---
+++ w10/d3/1-practice-for-week-10-sql-practice-sqlite3-cli-main/sql-practice/solution.sql
@@ -0,0 +1,18 @@
+-- STEP 1
+.help
+
+-- STEP2
+.tables
+.schema
+.read example.sql
+.tables
+.schema
+SELECT * FROM samples;
+
+-- STEP 3
+.headers on
+-- for prettier output run: .mode column
+SELECT * FROM samples;
+
+-- STEP 4
+Cbtrl+d or .exit
