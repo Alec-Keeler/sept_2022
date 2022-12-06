f3af2b99393748d7e16c736ab94f0a9872f96216
Jhass Theisen <jhasstheisen1@gmail.com>
2022-12-06 11:06:48 -0800

    finish seeding

---
+++ w11/d2/5-practice-for-week-11-sequelize-seeders-short-practice-main/server/db/seeders/20221206185526-fancy-colors.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Colors', [{
+      name: 'babyBlue',
+      createdAt: new Date('December 17, 1995 03:24:00'),
+      updatedAt: new Date('December 16, 1995 03:24:00')
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Colors', {
+      name: ['babyBlue']
+    })
+  }
+};
