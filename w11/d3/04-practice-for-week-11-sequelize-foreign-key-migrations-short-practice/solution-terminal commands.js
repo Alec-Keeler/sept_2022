22ab7301cd594e5f84dc383b3e9148d6add99962
Nathan Treadaway <natetread@gmail.com>
2022-12-07 13:51:00 -0500

    FK Migrations Completed

---
+++ w11/d3/04-practice-for-week-11-sequelize-foreign-key-migrations-short-practice/solution-terminal commands.js
@@ -0,0 +1,4 @@
+// make sure to cd into server before running these comands
+
+// npx sequelize-cli migration:generate --name add-bandId-to-musicians
+// npx sequelize model: generate--name MusicianInstrument--attributes musicianId: integer, instrumentId: integer
