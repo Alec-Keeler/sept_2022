ed00af59e44a094ee25ea5d59d39592ba19507dc
Alec Keeler <alec.keeler@gmail.com>
2022-12-15 12:27:52 -0600

    w12d4 complete

--- wk12/d4/1-practice-for-week-12-jwt-exploration-short-practice-main/app.js
+++ wk12/d4/1-practice-for-week-12-jwt-exploration-short-practice-main/app.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // Import package
-
+const jwt = require('jsonwebtoken');
 // Your code here
 
 // Define variables - DO NOT MODIFY
@@ -10,7 +10,11 @@ let token;
 let payload;
 
 // 1. Sign (create) a JWT containing your email address
-
+token = jwt.sign({
+    email: 'alec@google.io',
+    username: 'alec'
+}, 
+process.env.SECRET_KEY,)
 // Your code here
 
 // See the JWT in the console
@@ -19,16 +23,16 @@ console.log('JWT:', token);
 // 2. Decode a JWT Payload
 
 // Your code here
-
+payload = jwt.decode(token)
 // See the decoded payload in the console
 console.log('Payload:', payload);
 
 // 3. Verify a JWT
 
 // Your code here
-
+let isSecure = jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsZWNAZ29vZ2xlLmlvIiwidXNlcm5hbWUiOiJhbGVjIiwiaWF0IjoxNjcxMTI3MjgxfQ.xh7ucIaioRNAnVqONkOixjThF-aFe4jFlVBpU8352Rk', process.env.SECRET_KEY)
 // See the verified payload in the console
-console.log('Verified Payload:', payload);
+console.log('Verified Payload:', isSecure);
 
 // (Optional) Bonus: Catch Error With Invalid Signature
 // Generate an alternate secret key and use it
--- wk12/d4/3-bcrypt/bcrypt_code.js
+++ wk12/d4/3-bcrypt/bcrypt_code.js
@@ -1,1 +1,20 @@
 // We'll play around with a bit of bcryptjs code here :)
+const bcrypt = require('bcryptjs');
+
+const hashPass = async(password) => {
+    const hash = await bcrypt.hash(password, 12)
+    console.log(hash)
+}
+
+// hashPass('password')
+
+// $2a$12$R9h/cIPz0gi.URNNX3kh2OPST9/PgBkqquzi.Ss7KIUgO2t0jWMUW
+// \__/\/ \____________________/\_____________________________/
+// Alg Cost      Salt (22)                       Hash
+
+const testPass = async(password, hash) => {
+    const isPass = await bcrypt.compare(password, hash)
+    console.log(isPass)
+}
+
+testPass('password', '$2a$12$ZUZ7G05nvb6l9F0ev26M..C4WWb3zRlrmBje8TGRVvCzQkqCRGwc.')
